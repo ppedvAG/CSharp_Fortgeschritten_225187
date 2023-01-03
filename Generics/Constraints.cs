68f0b9d0a1daaeea04545fddd9d8176ebefe427d
ppedvLukasK <lukask@ppedv.de>
2023-01-03 10:20:04 +0100

    M003

---
+++ Generics/Constraints.cs
@@ -0,0 +1,39 @@
+﻿namespace Generics;
+
+internal class Constraints
+{
+	static void Main(string[] args)
+	{
+		DataStore4<Constraints> ds4;
+		DataStore5<DayOfWeek> ds5;
+	}
+
+	public class DataStore1<T> where T : class { } //T muss ein Referenztyp sein
+
+	public class DataStore2<T> where T : struct { } //T muss ein Wertetyp sein
+
+	public class DataStore3<T> where T : Program { } //T muss die Klasse selber sein oder eine Unterklasse sein
+
+	public class DataStore4<T> where T : new() { } //T muss einen Standardkonstruktor haben
+
+	public class DataStore5<T> where T : Enum { } //T muss ein Enum sein (kein Enumwert)
+
+	public class DataStore6<T> where T : Delegate { } //T muss ein Delegate sein
+
+	public class DataStore7<T> where T : unmanaged { } //Nur Basisdatentypen und ein paar weitere Typen
+	//https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/unmanaged-types
+
+	public class DataStore8<T> where T : class, Enum, new() { } //Mehrere Constraints auf ein Generic
+
+	public class DataStore9<T1, T2>
+		where T1 : class
+		where T2 : struct
+	{ }
+
+	public class DataStore10<T1, T2, T3, T4, T5, T6, T7> { } //Beliebig viele Generics möglich
+
+	public void Test<T>() where T : unmanaged //Constraints bei Methode hinzufügen
+	{
+
+	}
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+
+namespace Generics;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		List<string> list = new List<string>(); //Generic: T wird nach unten übernommen (hier T = string)
+		list.Add("123"); //T wird durch string ersetzt Add(T) -> Add(string)
+
+		List<int> ints = new List<int>(); //T wird durch int ersetzt
+		ints.Add(123); //Add(T) -> Add(int)
+
+		Dictionary<string, int> keyValuePairs = new Dictionary<string, int>(); //Klasse mit 2 Generics: TKey -> string, TValue -> int
+		keyValuePairs.Add("123", 123);
+	}
+}
+
+public class DataStore<T>
+	: IProgress<T>, //T wird bei Vererbung weitergegeben
+	  IEquatable<int> //int statt T als fixer Typ
+{
+	private T[] data; //T als Typ
+
+	public List<T> Data => data.ToList(); //Generic wird nach unten weitergegeben
+
+	public void Add(int index, T value) //T als Parameter
+	{
+		data[index] = value;
+	}
+
+	public T Get(int i) //T als Rückgabewert
+	{
+		if (i < 0 || i > data.Length)
+			return default(T); //default(T): Standardwert von T (int: 0, string: null, bool: false, ...)
+		return data[i];
+	}
+
+	public void Report(T value) //T kommt von Interface
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool Equals(int other)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void PrintType<MyType>() //Methode mit Generic, T von oben nicht nochmal definieren
+	{
+		Console.WriteLine(default(MyType)); //default(T): Standardwert von T (int: 0, string: null, bool: false, ...)
+		Console.WriteLine(typeof(MyType)); //Typ Objekt aus dem Generic erzeugen
+		Console.WriteLine(nameof(MyType)); //Gibt den Namen des Typs aus ("int", "string", "bool", ...)
+
+		//if (MyType is int) { } //Nicht möglich
+
+		if (typeof(MyType) == typeof(int)) //Typvergleiche mit Generics
+		{
+
+		}
+	}
+}
+
+public class DataStore2<T> : DataStore<T> { } //Klassen mit T vererben: braucht wieder T beim Klassennamen
